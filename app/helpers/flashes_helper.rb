5bced96c8c286484d28228ce26b1cf0b785314c8
huangpuxiang <hpx0809@outlook.com>
2017-11-16 23:56:23 +0800

    implement navbar and footer and flash

---
+++ app/helpers/flashes_helper.rb
@@ -0,0 +1,12 @@
+module FlashesHelper
+  FLASHES_CLASS = { :notice => "success", :warning => "warning", :alert => "danger"}
+
+  def flashes_class(key)
+    FLASHES_CLASS.fetch(key.to_sym, key)
+  end
+
+  def user_facing_flashes
+    flash.to_hash.slice("notice", "warning", "alert").freeze
+  end
+
+end
